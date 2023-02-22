23adcab30bcdba74e7774396022c6b5854633a50
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-02-22 15:38:27 +0600

    22/02/23

---
+++ triangle.js
@@ -0,0 +1,8 @@
+function triangleFun(triangle) {
+  for (let i = triangle.length - 2; i >= 0; i--)
+    for (let j = 0; j < triangle[i].length; j++)
+      triangle[i][j] += Math.min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+  return triangle[0][0];
+}
+
+console.log(triangleFun([[-1], [2, 3], [1, -1, -3]]));
