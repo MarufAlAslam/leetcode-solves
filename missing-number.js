32b5bbbe267d8b1744fdb43ba74e32ab2aaee167
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-02-27 18:34:52 +0600

    27/02/23

--- missing-number.js
+++ missing-number.js
@@ -1,12 +1,15 @@
 function missingNumber(arr) {
   let missing = 0;
+  // create a new array from 1 to arr.length
+  let refArray = Array.from({ length: arr.length }, (v, i) => i + 1);
 
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 1; j <= arr.length; j++) {
-      console.log("i: ", i, "j: ", j);
+  // compare 2 arrays to find the missing number
+  for (let i = 0; i < refArray.length; i++) {
+    if (arr.indexOf(refArray[i]) === -1) {
+      missing = refArray[i];
     }
-    continue;
   }
+
   return missing;
 }
 
