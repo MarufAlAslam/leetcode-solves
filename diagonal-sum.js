d42f30d88212be65a9f1001ed665057a15a97cce
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-03-20 18:42:40 +0600

    20-03-23

---
+++ diagonal-sum.js
@@ -0,0 +1,20 @@
+function diagonalSum(matrix) {
+  const diagonalArray = [];
+  let sum;
+  for (let i = 0; i < matrix.length; i++) {
+    diagonalArray.push(matrix[i][i]);
+    matrix[i][i] = 0;
+    diagonalArray.push(matrix[i][matrix.length - 1 - i]);
+  }
+  console.log(matrix);
+  return diagonalArray.reduce((a, b) => a + b);
+}
+
+console.log(
+  diagonalSum([
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+  ])
+);
