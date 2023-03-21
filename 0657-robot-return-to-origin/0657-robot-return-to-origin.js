f35ff8d06075b35fcaefe6a6587cb28a15cfbe55
Maruf H <maruf14@cse.pstu.ac.bd>
2023-03-21 16:53:03 +0600

    Time: 66 ms (86.24%), Space: 45.5 MB (16.01%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function robotReturnedToOrigin(moves) {
+  let x = 0;
+  let y = 0;
+  const movesArray = moves.split("");
+
+  for (let i = 0; i < movesArray.length; i++) {
+    if (movesArray[i] === "U") {
+      y = y + 1;
+    } else if (movesArray[i] === "D") {
+      y = y - 1;
+    } else if (movesArray[i] === "L") {
+      x = x - 1;
+    } else if (movesArray[i] === "R") {
+      x = x + 1;
+    }
+  }
+
+  return x === 0 && y === 0;
+}
