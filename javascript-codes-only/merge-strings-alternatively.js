82fbbeca37f469fe23500b3e9856b979ca09d6c7
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-03-21 16:12:31 +0600

    21/03/23

---
+++ javascript-codes-only/merge-strings-alternatively.js
@@ -0,0 +1,23 @@
+function mergeStrings(str1, str2) {
+  const str1Array = str1.split("");
+  const str2Array = str2.split("");
+  const modifiedStr2Array = [];
+  const remaining = [];
+  const result = [];
+
+  for (let i = 0; i < str1Array.length; i++) {
+    result.push(str1Array[i]);
+    if (str2Array[i]) {
+      result.push(str2Array[i]);
+      modifiedStr2Array.push(str2Array[i]);
+    }
+  }
+
+  for (let i = modifiedStr2Array.length; i < str2Array.length; i++) {
+    remaining.push(str2Array[i]);
+  }
+
+  return [...result, ...remaining].join("");
+}
+
+console.log(mergeStrings("cf", "eee")); // "apbqcr"
