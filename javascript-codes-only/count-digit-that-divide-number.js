701ebdacbb89d48548e0f64854c8db7cbbbcbe2a
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-03-22 16:07:27 +0600

    22/03/23

---
+++ javascript-codes-only/count-digit-that-divide-number.js
@@ -0,0 +1,12 @@
+function countDigit(num) {
+  const numArray = num.toString().split("");
+  let count = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    if (num % parseInt(numArray[i]) === 0) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countDigit(1248));
