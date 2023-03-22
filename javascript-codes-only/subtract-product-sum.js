df03d1f1212c1e1f3c8517c7fc779f09a78f73ba
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-03-22 15:57:19 +0600

    22/03/23

---
+++ javascript-codes-only/subtract-product-sum.js
@@ -0,0 +1,14 @@
+function subProductSum(n) {
+  const numArray = n.toString().split("");
+  let sum = 0;
+  let mul = 1;
+
+  for (let i = 0; i < numArray.length; i++) {
+    sum = sum + parseInt(numArray[i]);
+    mul = mul * parseInt(numArray[i]);
+  }
+
+  return mul - sum;
+}
+
+console.log(subProductSum(234));
