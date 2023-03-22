eadf18d3445fe2d896e0a302d245008035a9b3cc
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-03-22 16:33:50 +0600

    22/03/23

---
+++ javascript-codes-only/color-of-chessboard.js
@@ -0,0 +1,23 @@
+function colorOfChessBoard(str) {
+  const splittedArray = str.split("");
+  if (
+    splittedArray[0] === "a" ||
+    splittedArray[0] === "c" ||
+    splittedArray[0] === "e" ||
+    splittedArray[0] === "g"
+  ) {
+    if (parseInt(splittedArray[1]) % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    if (parseInt(splittedArray[1]) % 2 === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+console.log(colorOfChessBoard("a1"));
