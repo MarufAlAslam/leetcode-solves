0472c816cc59405383d15395a4f14e212ae61d49
Maruf H <maruf14@cse.pstu.ac.bd>
2023-04-10 13:31:37 +0600

    Time: 76 ms (53.29%), Space: 42.8 MB (60.97%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function isomorphicStrings(s, t) {
+    if (s.length !== t.length) return false;
+    let map = new Map();
+    for (let i = 0; i < s.length; i++) {
+        if (map.has(s[i])) {
+            if (map.get(s[i]) !== t[i]) return false;
+        } else {
+            if (Array.from(map.values()).includes(t[i])) return false;
+            map.set(s[i], t[i]);
+        }
+    }
+    return true;
+}
