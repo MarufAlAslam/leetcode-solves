dc2ab7f8dc1d28adc030a182ed830f661466c0b8
Maruf H <maruf14@cse.pstu.ac.bd>
2023-04-15 14:33:42 +0600

    Time: 90 ms (58.06%), Space: 45.8 MB (42.36%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function ransomNote(note, magazine) {
+    const noteArr = note.split("");
+    const magazineArr = magazine.split("");
+    const magazineObj = {};
+
+    magazineArr.forEach((letter) => {
+        if (!magazineObj[letter]) magazineObj[letter] = 0;
+        magazineObj[letter]++;
+    });
+
+    let noteIsPossible = true;
+    noteArr.forEach((letter) => {
+        if (magazineObj[letter]) {
+            magazineObj[letter]--;
+            if (magazineObj[letter] < 0) noteIsPossible = false;
+        } else noteIsPossible = false;
+    });
+
+    return noteIsPossible;
+}
