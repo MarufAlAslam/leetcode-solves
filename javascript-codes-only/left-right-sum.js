20b30d2a9f8d007c6e00c1b579c449296b5cb476
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-04-15 13:46:52 +0600

    15/04/23

---
+++ javascript-codes-only/left-right-sum.js
@@ -0,0 +1,28 @@
+function leftRightSum(arr) {
+    let leftSum = 0;
+    let rightSum = 0;
+    let leftSumArr = [];
+    let rightSumArr = [];
+    let res = []
+
+    leftSumArr[0] = 0;
+    rightSumArr[arr.length - 1] = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+        leftSum += arr[i - 1];
+        leftSumArr[i] = leftSum;
+    }
+
+    for (let i = arr.length - 2; i >= 0; i--) {
+        rightSum += arr[i + 1];
+        rightSumArr[i] = rightSum;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        res[i] = Math.abs(leftSumArr[i] - rightSumArr[i])
+    }
+
+    return res
+}
+
+console.log(leftRightSum([10, 4, 8, 3]));
