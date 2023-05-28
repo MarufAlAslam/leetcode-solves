acf19e888c7357b7bb012a8efd11175ad7f6b863
Maruf H <maruf14@cse.pstu.ac.bd>
2023-05-28 16:59:48 +0600

    Time: 82 ms (21.04%), Space: 43.7 MB (66.64%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var sumOfMultiples = function sumMultiples (num){
+    let sum = 0
+    for (let i = 1 ; i<= num; i++){
+        if(i % 3 === 0 || i % 5 === 0 || i % 7 === 0){
+            sum += i
+        }
+    }
+    return sum
+}
