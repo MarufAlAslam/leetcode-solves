1a0356c01695c41d0fb8c5a9e1ddf6c39bc6a048
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-06-25 16:48:10 +0600

    25/6/23

---
+++ javascript-codes-only/perfect-number.js
@@ -0,0 +1,13 @@
+function perfectNumber(num){
+    const divisors = []
+    const flag = false
+    for (let i = 1; i < num; i++){
+        if (num % i === 0){
+            divisors.push(i)
+        }
+    }
+
+    return divisors.reduce((acc, curr) => acc + curr, 0) === num ? true : false
+}
+
+console.log(perfectNumber(28))
