372f1ca3b5400a4e4bcb827e765fc9a1f3c417e0
Maruf H <maruf14@cse.pstu.ac.bd>
2023-07-03 17:00:57 +0600

    Time: 60 ms (52.65%), Space: 42.3 MB (21.21%) - LeetHub

---
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function createArray (nums, index){
+    const result = []
+    for (let i = 0 ; i < nums.length; i++){
+        result.splice(index[i], 0, nums[i])
+    }
+    return result
+}
