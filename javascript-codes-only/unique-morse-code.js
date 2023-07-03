578cee5ee55dce210fbf7b0448bb20a567579da0
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-07-03 17:29:47 +0600

    3/7/23

---
+++ javascript-codes-only/unique-morse-code.js
@@ -0,0 +1,17 @@
+function uniqueMorseCode(words){
+    const morseCode = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+    const alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
+    
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i].split("")
+        for (let j = 0; j < word.length; j++) {
+            const index = alphabet.indexOf(word[j])
+            word[j] = morseCode[index]
+        }
+        words[i] = word.join("")
+    }
+
+    return [...new Set(words)].length
+}
+
+console.log(uniqueMorseCode(["gin", "zen", "gig", "msg"]))
