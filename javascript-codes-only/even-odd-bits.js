1bd95d900440d2f84f92ab345ee66bc8f0a98656
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-07-11 12:51:01 +0600

    11/7/23

---
+++ javascript-codes-only/even-odd-bits.js
@@ -0,0 +1,17 @@
+var evenOddBit = function(n) {
+    const ans = [0, 0];
+    const binary = n.toString(2).split('').reverse();
+    
+    for (let i = 0; i < binary.length; i++) {
+        if (binary[i] === '1') {
+            if (i % 2) {
+                ans[1]++;
+            } else {
+                ans[0]++;
+            }
+        }
+    }
+    
+    return ans;
+};
+console.log(evenOddBit(2)) // 10111
