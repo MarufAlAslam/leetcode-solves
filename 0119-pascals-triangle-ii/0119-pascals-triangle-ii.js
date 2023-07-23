69beabfb45ec5f8d9265ab57e53d4379a242c19f
Maruf H <maruf14@cse.pstu.ac.bd>
2023-07-23 16:24:17 +0600

    Time: 50 ms (93.66%), Space: 43.5 MB (6.87%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function pascalsTriangle(n){
+    const res = [[1]]
+    for(let i = 1; i <= n; i++){
+        res[i] = [1, ...res[i-1].map((v, i, arr) => arr[i+1] ? arr[i] + arr[i+1] : 1)]
+    }
+    return res[n]
+}
