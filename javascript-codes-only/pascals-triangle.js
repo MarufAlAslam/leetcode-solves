c7d4ee69c8dd422a5157d74e59d73d72b7259cd6
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-07-23 16:21:52 +0600

    23/7/23

---
+++ javascript-codes-only/pascals-triangle.js
@@ -0,0 +1,10 @@
+function pascalsTriangle(n){
+    const res = [[1]]
+    for(let i = 1; i < n; i++){
+        res[i] = [1, ...res[i-1].map((v, i, arr) => arr[i+1] ? arr[i] + arr[i+1] : 1)]
+    }
+    return res
+}
+
+
+console.log(pascalsTriangle(5))
