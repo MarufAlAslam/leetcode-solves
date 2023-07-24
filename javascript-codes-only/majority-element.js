88d8f745cb0fc722f29c0edb24d81ba7177f7320
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-07-24 17:16:54 +0600

    24/7/23

---
+++ javascript-codes-only/majority-element.js
@@ -0,0 +1,17 @@
+function majorityElement(nums){
+    let obj = {}
+    for(let i = 0; i < nums.length; i++){
+        if(obj[nums[i]]){
+            obj[nums[i]]++
+        } else {
+            obj[nums[i]] = 1
+        }
+    }
+    for(let key in obj){
+        if(obj[key] > nums.length/2){
+            return key
+        }
+    }
+}
+
+console.log(majorityElement([2,2,1,1,1,2,2]))
