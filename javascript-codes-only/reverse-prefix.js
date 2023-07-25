ae6db11d24a857da51efe5dbb28c1e18c9299c3c
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-07-25 17:07:56 +0600

    25/7/23

---
+++ javascript-codes-only/reverse-prefix.js
@@ -0,0 +1,26 @@
+function reversePrefix(str, ch) {
+  const temp = [];
+  const splitted = str.split("");
+
+  if (!splitted.includes(ch)) {
+    return str;
+  } else {
+    let i = 0;
+    while (true) {
+      if (splitted[i] === ch) {
+        temp.push(splitted[i]);
+        break;
+      } else {
+        temp.push(splitted[i]);
+        i++;
+      }
+    }
+
+    return temp
+      .reverse()
+      .concat(splitted.slice(i + 1))
+      .join("");
+  }
+}
+
+console.log(reversePrefix("abcd", "z")); // "dcbaefd"
