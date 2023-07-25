2291c2607f0b155a9d752b7ae9073c0648abc5ac
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-07-25 17:10:21 +0600

    25/7/23

--- javascript-codes-only/detect-capital.js
+++ javascript-codes-only/detect-capital.js
@@ -1,23 +1,17 @@
 function detectCapital(str) {
   const strArray = str.split("");
-  let flag = 0;
+ 
 
-  for (let i = 0; i < strArray.length; i++) {
-    if (strArray[i] >= "A" && strArray[i] <= "Z") {
-      flag++;
-    } else if (strArray[0] >= "A" && strArray[0] <= "Z") {
-      flag = 1;
-    } else if (strArray[i] >= "a" && strArray[i] <= "z") {
-      flag = 1;
-    } else {
-      flag = 0;
-    }
+  return strArray.every((char) => {
+    return char === char.toUpperCase();
   }
-
-  if (flag === 1) {
-    return true;
+  ) || strArray.every((char) => {
+    return char === char.toLowerCase();
   }
-  return false;
+  ) || (strArray[0] === strArray[0].toUpperCase() && strArray.slice(1).every((char) => {
+    return char === char.toLowerCase();
+  }
+  ));
 }
 
-console.log(detectCapital("FlaG"));
+console.log(detectCapital("USA"));
