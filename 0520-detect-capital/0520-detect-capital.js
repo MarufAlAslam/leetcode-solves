2b8356f5e6b9466ae527c957ac11eccd228795f1
Maruf H <maruf14@cse.pstu.ac.bd>
2023-07-25 17:10:14 +0600

    Time: 63 ms (58.91%), Space: 42 MB (81.90%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function detectCapital(str) {
+  const strArray = str.split("");
+ 
+
+  return strArray.every((char) => {
+    return char === char.toUpperCase();
+  }
+  ) || strArray.every((char) => {
+    return char === char.toLowerCase();
+  }
+  ) || (strArray[0] === strArray[0].toUpperCase() && strArray.slice(1).every((char) => {
+    return char === char.toLowerCase();
+  }
+  ));
+}
