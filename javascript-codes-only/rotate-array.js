98c0e3bf434db85c1f8bbc180400f988eb092d0a
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-07-26 16:58:53 +0600

    26/7/23

--- javascript-codes-only/rotate-array.js
+++ javascript-codes-only/rotate-array.js
@@ -1,15 +1,21 @@
-function rotateArray(arr, k) {
-  let i = 0;
+function rotateArray(nums, k) {
+  k %= nums.length;
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
 
-  while (i < k) {
-    let end = arr.pop();
-    arr.unshift(end);
-    i++;
+function reverse(nums, start, end) {
+  while (start < end) {
+    const temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++;
+    end--;
   }
+}
 
-    return arr;
+  return nums;
 }
 
 // console.log(rotateArray([1,2,3,4,5,6,7], 3))
 console.log(rotateArray([1, 2], 3));
-// [5,6,7,1,2,3,4]
