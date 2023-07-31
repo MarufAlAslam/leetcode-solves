41f41b4cccfcb75c95b3216bff090acaa7e7f487
Maruf H <maruf14@cse.pstu.ac.bd>
2023-07-31 16:42:37 +0600

    Time: 55 ms (93.07%), Space: 44.1 MB (39.60%) - LeetHub

---
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var countEven = function evenDigitSum(num){
+    let count = 0;
+    for (let i = 1 ; i<=num ; i++){
+        let sum = 0;
+        let digits = i.toString().split('');
+        for (let j = 0 ; j<digits.length ; j++){
+            sum += parseInt(digits[j]);
+        }
+        if (sum%2===0){
+            count++;
+        }
+    }
+    return count
+}
