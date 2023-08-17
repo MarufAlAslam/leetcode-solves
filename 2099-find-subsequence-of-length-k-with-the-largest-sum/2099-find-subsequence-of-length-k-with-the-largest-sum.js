a9d716f00084a71b090496c73fba0e8bf56e2a41
Maruf H <maruf14@cse.pstu.ac.bd>
2023-08-17 17:41:23 +0600

    Time: 60 ms (89.41%), Space: 44.3 MB (61.18%) - LeetHub

---
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSubsequence = function lengthKOfLargeSum(nums, k){
+    nums = nums.map((n,i) => [i, n])
+    nums.sort((a,b) => b[1] - a[1])
+    let arr = nums.slice(0, k)
+    let res = []
+    for(let i of arr) {
+        res[i[0]] = i[1]
+    }
+    return res.filter(i=>i !== undefined)
+}
