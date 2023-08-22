6f2b73b8cbd6e8b38850a0f090043d68946e22ac
Maruf H <maruf14@cse.pstu.ac.bd>
2023-08-22 15:30:57 +0600

    Time: 59 ms (41.53%), Space: 41.9 MB (78.81%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfUnique = function sumOfUniqueElement(nums) {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let isUnique = true;
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] === nums[j] && i !== j) {
+        isUnique = false;
+        break;
+      }
+    }
+    if (isUnique) {
+      sum += nums[i];
+    }
+  }
+  return sum;
+}
