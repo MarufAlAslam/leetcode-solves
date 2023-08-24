bba16577dca6dcd3f53ec8b062997ad3d1f16bfb
Maruf H <maruf14@cse.pstu.ac.bd>
2023-08-24 15:04:17 +0600

    Time: 66 ms (33.49%), Space: 45.4 MB (41.57%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function reverseString(str, k) {
+  let resultStr = "";
+  const splitted = str.split("");
+
+  let k2 = 2 * k;
+
+  for (let i = 0; i < splitted.length; i = i + k2) {
+    const sliced = splitted.slice(i, i + k);
+    const reversed = sliced.reverse();
+    resultStr += reversed.join("");
+    const sliced2 = splitted.slice(i + k, i + 2 * k);
+    resultStr += sliced2.join("");
+  }
+
+    return resultStr;
+}
