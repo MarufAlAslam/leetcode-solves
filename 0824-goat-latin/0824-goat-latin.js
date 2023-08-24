b9982e31d011eeade0292fdca4faca4f82d7f807
Maruf H <maruf14@cse.pstu.ac.bd>
2023-08-24 15:32:59 +0600

    Time: 54 ms (61.95%), Space: 43.9 MB (6.83%) - LeetHub

---
+++ 0824-goat-latin/0824-goat-latin.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} sentence
+ * @return {string}
+ */
+var toGoatLatin = function goatLatin(sentence){
+    let splittedSentence = sentence.split(" ")
+    for(let i = 0 ; i<splittedSentence.length; i++){
+        if(
+            splittedSentence[i][0] === 'a' ||
+            splittedSentence[i][0] === 'A' ||
+            splittedSentence[i][0] === 'e' ||
+            splittedSentence[i][0] === 'E' ||
+            splittedSentence[i][0] === 'i' ||
+            splittedSentence[i][0] === 'I' ||
+            splittedSentence[i][0] === 'o' ||
+            splittedSentence[i][0] === 'O' ||
+            splittedSentence[i][0] === 'u' ||
+            splittedSentence[i][0] === 'U'
+        ){
+            splittedSentence[i] += "ma"
+        }
+        else{
+            splittedSentence[i] = splittedSentence[i].slice(1) + splittedSentence[i][0] + "ma"
+        }
+        for(let j = 0; j<=i; j++){
+            splittedSentence[i] += "a"
+        }
+    }
+    return splittedSentence.join(" ")
+}
