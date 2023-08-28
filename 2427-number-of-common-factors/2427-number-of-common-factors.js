713a1eb31ba7b7017ee09eda7806d0ce5ecaa42b
Maruf H <maruf14@cse.pstu.ac.bd>
2023-08-28 17:44:11 +0600

    Time: 60 ms (33.33%), Space: 41.6 MB (88.72%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var commonFactors = function countCommonFactors(a, b){
+    let count = 0;
+    if(a < b){
+        for(let i = 1; i <= a; i++){
+            if(a % i === 0 && b % i === 0){
+                count++;
+            }
+        }
+    }
+    else{
+        for(let i = 1; i <= b; i++){
+            if(a % i === 0 && b % i === 0){
+                count++;
+            }
+        }
+    }
+    return count
+}
